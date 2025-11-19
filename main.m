c70e60379c0a03c5704781d9209228d08b041aa8
rogolop <45009376+rogolop@users.noreply.github.com>
2025-11-19 19:05:20 +0100

    Add procedures that print the Enriques diagram and dual graph with given annotations

--- main.m
+++ main.m
@@ -19,7 +19,7 @@ case 2:
 		semigroup := [10,15,36]; //[8,12,26,53]; //[6,7]; //[10,15,36];
 	when 2:
 		// a>=2, b>=a+1, c>=2, d>=1, {a,b,c} pairwise coprime, {c,d} coprime
-		abcd := [2,3,5,2]; //[3,8,7,1]; //[4,7,3,1];
+		abcd := [2,3,5,2]; // [2,3,5,2] [3,8,7,1] [4,7,3,1]
 end case;
 
 //P<x,y> := LocalPolynomialRing(Q,2); R:=Q;
@@ -40,6 +40,56 @@ if not assigned f then
 end if;
 P2<X,Y> := PolynomialRing(Q,2);
 
+prt:=procedure(L)printf"[";for i->l in L do printf"%o%o",&cat Split(Sprintf("%o",l),"*"),i lt#L select", "else"";end for;printf"]";end procedure;
+
+procedure printAnnotatedDualGraph(dualGraphMatrix,annotations : vertSep:="", horizSep:="---")
+	for j in [1..Ncols(dualGraphMatrix)] do
+		if j gt 1 then printf vertSep; end if;
+		for i in [1..Nrows(dualGraphMatrix)] do
+			pt := dualGraphMatrix[i][j];
+			if pt eq 0 then break; end if;
+			if i gt 1 then printf horizSep; end if;
+			printf "%o", annotations[pt];
+		end for;
+		printf "\n";
+	end for;
+end procedure;
+
+procedure printAnnotatedEnriquesDiagram(enriques, annotations)
+	maxLabelSize := Max([Z| #Sprintf("%o",ann) : ann in Eltseq(annotations)]);
+	for i in [1..Nrows(enriques)] do
+	for j in [1..Ncols(enriques)] do
+		case enriques[i][j]:
+		when 0:
+			printf " " cat " "^maxLabelSize;
+		when -1:
+			printf "─"^Ceiling((maxLabelSize)/ 2) cat "╯" cat
+				" "^Floor((maxLabelSize)/ 2);
+		when -2:
+			printf " "^Ceiling((maxLabelSize)/ 2) cat "│" cat
+				" "^Floor((maxLabelSize)/ 2);
+		when -3:
+			printf " "^Ceiling((maxLabelSize)/ 2) cat "╭" cat
+				"─"^Floor((maxLabelSize)/ 2);
+		else:
+			ann := Sprintf("%o",annotations[enriques[i][j]]);
+			prefix := " ";
+			if j gt 1 then
+				if i eq 1 then
+					prefix := "~";
+				else
+					if enriques[i][j-1] gt 0 then
+						prefix := "─";
+					end if;
+				end if;
+			end if;
+			printf "%o%o", &cat[prefix:k in [1..1+maxLabelSize-#ann]], ann;
+		end case;
+	end for;
+	printf "\n";
+	end for;
+end procedure;
+
 printf "\nSemigroup %o\n", semigroup;
 g := #semigroup -1;
 mu := MilnorNumber(f);
@@ -58,6 +108,8 @@ M := N;
 for i in [1..numPoints] do
 	M[i][i] := 0;
 end for;
+isFree := [ &+Eltseq(Prf[pt]) ge 0 : pt in [1..numPoints]];
+isRupture := [ (&+Eltseq(M[pt]) ge 3) or (pt eq numPoints and &+Eltseq(M[pt]) ge 2) : pt in [1..numPoints]];
 
 plotPrf := ZeroMatrix(Z, numPoints);
 for i in [1..numPoints], j in [1..i-1] do
@@ -73,36 +125,37 @@ for i in [1..numPoints], j in [1..i-1] do
 end for;
 
 //dualGraphEdges := [];
-//for i in [1..numPoints] do for j in [1..i] do
+//for i in [1..numPoints], j in [1..i] do
 //	if M[i][j] eq 1 then Append(~dualGraphEdges, <j,i>); end if;
-//end for; end for;
-// Reorder points for dual graph
-neighbors := [];
-parents := [Z| ];
+//end for;
+
+// Calculate dual graph
+children := [];
+parent := [Z| ];
 visited := {Z| }; pending := {Z| 1}; // temp
 while #visited lt numPoints do
 	pt := Representative(pending);
 	Exclude(~pending, pt);
 	Include(~visited, pt);
-	neighbors[pt] := [Z| i : i in [1..numPoints] | M[pt][i] eq 1 and i notin visited];
-	for neigh in neighbors[pt] do
-		Include(~pending, neigh);
-		parents[neigh] := pt;
+	children[pt] := [Z| i : i in [1..numPoints] | M[pt][i] eq 1 and i notin visited];
+	for child in children[pt] do
+		Include(~pending, child);
+		parent[child] := pt;
 	end for;
 end while;
 mainRow := [Z| numPoints];
 while mainRow[#mainRow] ne 1 do
-	Append(~mainRow, parents[mainRow[#mainRow]]);
+	Append(~mainRow, parent[mainRow[#mainRow]]);
 end while;
 Reverse(~mainRow);
 dualGraphBranches := [[Z| ] : i in [1..numPoints]];
 for pt in mainRow do
-	nonMainNeighbors := [Z| neigh : neigh in neighbors[pt] | neigh notin mainRow];
+	nonMainNeighbors := [Z| child : child in children[pt] | child notin mainRow];
 	if #nonMainNeighbors gt 0 then
 		pt2 := nonMainNeighbors[1];
 		dualGraphBranches[pt] := [Z| pt2];
-		while #neighbors[pt2] gt 0 do
-			pt2 := neighbors[pt2][1];
+		while #children[pt2] gt 0 do
+			pt2 := children[pt2][1];
 			Append(~dualGraphBranches[pt], pt2);
 		end while;
 	end if;
@@ -115,71 +168,98 @@ for i->pt in mainRow do
 	end for;
 end for;
 
+
+// Calculate enriques diagram
+enriques := ZeroMatrix(Z, 2*numPoints);
+i:=1; j:=1; di:=0; dj:=1;
+lastProximateTo := 0;
+enriques[1][1] := 1;
+lastWasSatellite := false;
+for pt in [2..numPoints] do
+	if isFree[pt] then
+		if lastWasSatellite then
+			j +:= 1;
+			enriques[i][j] := -1;
+			while i gt 1 do
+				i -:= 1;
+				enriques[i][j] := -2;
+			end while;
+			enriques[i][j] := -3;
+		end if;
+		lastWasSatellite := false;
+		i:=1; di:=0; dj:=1;
+	else
+		proximateTo := Min([Z| k : k in [1..numPoints] | Prf[pt][k] eq -1]);
+		if lastProximateTo ne proximateTo then
+			di, dj := Explode(<dj,di>);
+		end if;
+		lastWasSatellite := true;
+	end if;
+	i +:= di; j +:= dj;
+	enriques[i][j] := pt;
+end for;
+actualHeight := Max([Z| k : k in [1..Nrows(enriques)] | &+Eltseq(enriques[k]) ne 0]);
+actualWidth := Max([Z| k : k in [1..Ncols(enriques)] | &+Eltseq(Transpose(enriques)[k]) ne 0]);
+enriques := Submatrix(enriques, 1,1, actualHeight, actualWidth);
+
+
+
+
+
+
+
+
+
+
+
+
 //print "Proximity matrix of f:"; print Prf;
 //printf "ef   = %o\n", ef;
 //printf "vf   = %o\n", vf;
 //printf "excf = %o\n", excf;
 //print "Intersection matrix of f:"; print N; printf "\n";
 //print M;
-//for j in [1..Ncols(dualGraphMatrix)] do
-//	if j gt 1 then printf "--"; end if;
-//	pt := dualGraphMatrix[1][j];
-//	printf "p%-2o(%-2o,%-3o)", pt, ef[1][pt], vf[1][pt];
-//end for;
-//printf "\n";
-//for i in [2..Nrows(dualGraphMatrix)] do
-//	for j in [1..Ncols(dualGraphMatrix)] do
-//		if 0 lt &+[Z| dualGraphMatrix[k][j] : k in [i..Nrows(dualGraphMatrix)]] then
-//			// something below
-//			if j gt 1 then printf "  "; end if;
-//			printf "|%-2o        ", " ";
-//		else
-//			// nothing below
-//			if j gt 1 then printf "  "; end if;
-//			printf " %-2o        ", " ";
-//		end if;
-//	end for;
-//	printf "\n";
-//	for j in [1..Ncols(dualGraphMatrix)] do
-//		if j gt 1 then printf "  "; end if;
-//		pt := dualGraphMatrix[i][j];
-//		if pt eq 0 then
-//			printf " %-2o        ", " ";
-//		else
-//			printf "p%-2o(%-2o,%-3o)", pt, ef[1][pt], vf[1][pt];
-//		end if;
-//	end for;
-//	printf "\n";
-//end for;
 //printf "\n"; print dualGraphMatrix;
 printf "\n";
-printf "Proximity:\n";
+printf "Proximity with (e,v):\n";
 boxDrawingCharacters := [" ", "╵", "╷", "│", "╶", "└", "┌", "├", "╴", "┘", "┐", "┤", "─", "┴", "┬", "┼"];
 for i in [1..numPoints], j in [1..i] do
-	if i eq j then printf "P%o (e%o,v%o)\n", i-1, ef[1][i], vf[1][i]; continue; end if;
+	if i eq j then printf "%o%o (%o,%o)\n", isRupture[i] select "r" else "p", i-1, ef[1][i], vf[1][i]; continue; end if;
 	// 1 up, 2 down, 4 right, 8 left
 	printf "%o", boxDrawingCharacters[plotPrf[i][j] +1];
 end for;
-
-printf "\n";
-printf "Dual graph:\n";
-formatString := "P%o(e%o,v%o)"; //"(P%-2o e%-2o,v%-4o)";
-for j in [1..Ncols(dualGraphMatrix)] do
-	pt := dualGraphMatrix[1][j];
-	if j gt 1 then printf " |\n |\n"; end if;
-	printf formatString, pt-1, ef[1][pt], vf[1][pt];
-	for pt2 in dualGraphBranches[pt] do
-		printf "---";
-		printf formatString, pt2-1, ef[1][pt2], vf[1][pt2];
-	end for;
-	printf "\n";
-end for;
-
 printf "\n";
 
+printf "Points:\n";
+annotations := [Sprintf("p%o", pt-1) : pt in [1..numPoints]];
+printAnnotatedEnriquesDiagram(enriques, annotations);
+printAnnotatedDualGraph(dualGraphMatrix, annotations);
+printf "\n";
+
+printf "Multiplicities (e):\n\n";
+annotations := ef[1];
+printAnnotatedEnriquesDiagram(enriques, annotations);
+printAnnotatedDualGraph(dualGraphMatrix, annotations);
+printf "\n";
+
+printf "Values (v):\n";
+annotations := vf[1];
+printAnnotatedEnriquesDiagram(enriques, annotations);
+printAnnotatedDualGraph(dualGraphMatrix, annotations);
+printf "\n";
+
+//printf "\nDual graph with (e,v) and *=rupture:\n";
+////printAnnotatedDualGraph(dualGraphMatrix, [Sprintf("p%o%o(%o,%o)", pt-1, isRupture[pt]select"*"else"_", ef[1][pt], vf[1][pt]) : pt in [1..numPoints]] : vertSep:="|\n");
+
+//printf "\n"; printAnnotatedDualGraph(dualGraphMatrix, [Sprintf("p%o(%o,%o)", pt-1, ef[1][pt], vf[1][pt]) : pt in [1..numPoints]] : horizSep:="___");
+
+//printf "\n"; printAnnotatedDualGraph(dualGraphMatrix, [Sprintf("%o%o~%o", isRupture[pt]select"*"else"_", ef[1][pt], vf[1][pt]) : pt in [1..numPoints]] : horizSep:="___");
+
+//printf "\n"; printAnnotatedDualGraph(dualGraphMatrix, [isRupture[pt]select"*"else"o": pt in [1..numPoints]]);
 
 
-prt:=procedure(L)printf"[";for i->l in L do printf"%o%o",&cat Split(Sprintf("%o",l),"*"),i lt#L select", "else"";end for;printf"]";end procedure;
+
+
 
 // Calculate
 
@@ -227,11 +307,32 @@ end for;
 
 // Compare filtration vs multipliers
 
-print "Columns:";
+print "Data:";
 print "- Filtration ideal is a multiplier ideal (Y/NO)";
 print "- Intersection multiplicity of the clusters?";
 print "- Jumping number, or ideal that is not a multiplier ideal";
+print "- dimension of C[x,y] / I";
+print "- Excesses";
 printf "\n";
+
+procedure printIdealData(I)
+	IndentPush(5);
+	I2 := ideal<P2| [Evaluate(gen,[X,Y]) : gen in Basis(I)]>;
+	printf "dim R/I = %o\n", Dimension(P2/I2);
+	//print LogResolution(I);
+	Pr, v := LogResolution(I);
+	e := v * Transpose(Pr);
+	exc := e * Pr;
+	annotations := Eltseq(exc) cat [Z|0:i in [Ncols(e)..numPoints]];
+	printAnnotatedEnriquesDiagram(enriques, annotations);
+	printAnnotatedDualGraph(dualGraphMatrix, annotations);
+	//printf "e = %o\n", e;
+	vUnload := Unloading(Transpose(Pr)*Pr,v);
+	eUnload := vUnload * Transpose(Pr);
+	//printf "vUnload = %o\n", vUnload;
+	//printf "eUnload = %o\n", eUnload;
+	IndentPop(5);
+end procedure;
 
 I124 := [];
 I127 := [];
@@ -248,17 +349,9 @@ for I in filtrationIdeals do
 			printf "   Y %-4o %-8o ", filtrationIdealToInt[I], multiplierIdealToJN[I];
 			if printCoincidingIdeals then
 				SNice, extras := MonomialSequence(filtrationIdealToSequence[I], maxContact);
-				prt(SNice); for i->elt in extras do printf "\ng%o = %o", i, elt; end for;
-				printf "\n"; print LogResolution(I);
-				Pr, v := LogResolution(I);
-				e := v * Transpose(Pr);
-				printf "e = %o\n", e;
-				vUnload := Unloading(Transpose(Pr)*Pr,v);
-				eUnload := vUnload * Transpose(Pr);
-				printf "vUnload = %o\n", vUnload;
-				printf "eUnload = %o\n", eUnload;
-				I2 := ideal<P2| [Evaluate(gen,[X,Y]) : gen in Basis(I)]>;
-				printf "dim R/I = %o\n", Dimension(P2/I2);
+				//prt(SNice); for i->elt in extras do printf "\ng%o = %o", i, elt; end for;
+				//printf "\n";
+				printIdealData(I);
 			end if;
 			printf "\n";
 		end if;
@@ -267,18 +360,9 @@ for I in filtrationIdeals do
 		// printf "        ";
 		if printExtraFiltrationIdeals then
 			SNice, extras := MonomialSequence(filtrationIdealToSequence[I], maxContact);
-			prt(SNice); for i->elt in extras do printf "\ng%o = %o", i, elt; end for;
-			
-			printf "\n"; print LogResolution(I);
-			Pr, v := LogResolution(I);
-			e := v * Transpose(Pr);
-			printf "e = %o\n", e;
-			vUnload := Unloading(Transpose(Pr)*Pr,v);
-			eUnload := vUnload * Transpose(Pr);
-			printf "vUnload = %o\n", vUnload;
-			printf "eUnload = %o\n", eUnload;
-			I2 := ideal<P2| [Evaluate(gen,[X,Y]) : gen in Basis(I)]>;
-			printf "dim R/I = %o\n", Dimension(P2/I2);
+			//prt(SNice); for i->elt in extras do printf "\ng%o = %o", i, elt; end for;
+			//printf "\n";
+			printIdealData(I);
 		end if;
 		printf "\n";
 	end if;
@@ -292,18 +376,9 @@ else
 		printf "JN=%-8o ", multiplierIdealToJN[I];
 		if printMissingMultiplierIdeals then
 			SNice, extras := MonomialSequence(multiplierIdealToSequence[I], maxContact);
-			prt(SNice); for i->elt in extras do printf "\ng%o = %o", i, elt; end for;
-			
-			printf "\n"; print LogResolution(I);
-			Pr, v := LogResolution(I);
-			e := v * Transpose(Pr);
-			printf "e = %o\n", e;
-			vUnload := Unloading(Transpose(Pr)*Pr,v);
-			eUnload := vUnload * Transpose(Pr);
-			printf "vUnload = %o\n", vUnload;
-			printf "eUnload = %o\n", eUnload;
-			I2 := ideal<P2| [Evaluate(gen,[X,Y]) : gen in Basis(I)]>;
-			printf "dim R/I = %o\n", Dimension(P2/I2);
+			//prt(SNice); for i->elt in extras do printf "\ng%o = %o", i, elt; end for;
+			//printf "\n";
+			printIdealData(I);
 		end if;
 		printf "\n";
 	end for;
